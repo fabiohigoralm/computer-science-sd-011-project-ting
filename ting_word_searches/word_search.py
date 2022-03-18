b023fc1642a9da274e076232975437b67b5139e1
fabiohigoralm <fabiohigoralm2@gmail.com>
2022-03-18 12:17:47 -0300

    Requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for data in instance.data:
+        for item, line in enumerate(data["linhas_do_arquivo"]):
+            if word in line:
+                results.append(
+                    {
+                        "palavra": word,
+                        "arquivo": data["nome_do_arquivo"],
+                        "ocorrencias": [
+                            {"linha": item + 1},
+                        ],
+                    }
+                )
+
+    return results
 
 
 def search_by_word(word, instance):
